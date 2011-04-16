d64124afb84274c599fe2f39bfb1458bc4434f22
bkinsey808 <bkinsey@gmail.com>
2011-04-16 01:05:57 -1000

    first commit

---
+++ bkjs.php
@@ -0,0 +1,15 @@
+<html>
+<head>
+  <title></title>
+</head>
+<body>
+<script type="text/javascript" src="http://ajax.aspnetcdn.com/ajax/jQuery/jquery-1.5.2.min.js"></script>
+<div id="header"></div>
+<div id="breadcrumb"></div>
+<div id="sidebar"></div>
+<div id="view"><a id="bar" ></a>
+Parameter: <?php print_r($_GET); ?>
+<script type="text/javascript" src="/bkjs.js"></script>
+</div>
+</body>
+</html>
